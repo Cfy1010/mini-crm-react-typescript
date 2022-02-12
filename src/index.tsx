6da40adde22abdeeb55eccb1664aa6f9ff7cc2f5
Cfy1010 <cy_one2000@yahoo.fr>
2022-02-12 12:36:33 +0100

    submit form avec des hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+type address = {
+  name: string;
+  email: string;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [adresses, setAdresses] = useState<address[]>([]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newAddress: address = {
+      name,
+      email,
+    };
+
+    setAdresses([...adresses, newAddress]);
+    setName('')
+    setEmail('')
+  };
+
+  return (
+    <>
+      <h1>App CRM</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Nom </label>
+        <input
+          id='name'
+          type='text'
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <br />
+        <label htmlFor='email'>Email </label>
+        <input
+          id='email'
+          type='text'
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <button type='submit'>Add</button>
+      </form>
+      <hr />
+      {adresses.length > 0 && (
+        <ul>
+          {adresses.map((element) => (
+            <li key={element.email}>
+              {element.name} : {element.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+ReactDOM.render(<App />, document.getElementById('root'));
